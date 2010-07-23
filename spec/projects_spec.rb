f6c4b24e28f1bb22aaf498a0002ce3f8add408e8
Simon <sirup@sirupsen.dk>
2010-07-23 17:38:33 +0200

    Trying to make the tests wilder

--- spec/projects_spec.rb
+++ spec/projects_spec.rb
@@ -9,38 +9,62 @@ describe "Unfuddler::Project" do
     end
   end
 
-  it "should be able to load all projects" do
-    Unfuddler::Project.find.should be_an_instance_of Array
-    Unfuddler::Project.find.first.should be_an_instance_of Unfuddler::Project
-  end
+  describe "find" do
+    describe "given no arguments" do
+      it "should return an Array" do
+        Unfuddler::Project.find.should be_an_instance_of Array
 
-  it "should be able to find projects by string as name" do
-    Unfuddler::Project.find("testproject").should be_an_instance_of Array
-    Unfuddler::Project.find("testproject").first.should be_an_instance_of Unfuddler::Project
-    Unfuddler::Project.find("testproject").first.short_name.should eql("testproject")
-  end
+      it "should return an Array with Unfuddler::Project"
+        Unfuddler::Project.find.first.should be_an_instance_of Unfuddler::Project
+      end
+    end
 
-  it "should be able to find projects by hash" do
-    Unfuddler::Project.find(:id => 2).should be_an_instance_of Array
-    Unfuddler::Project.find(:id => 2).first.should be_an_instance_of Unfuddler::Project
-    Unfuddler::Project.find(:id => 2).first.short_name.should eql("testproject")
-  end
+    describe "given one string argument" do
+      it "should return an Array" do
+        Unfuddler::Project.find("testproject").should be_an_instance_of Array
+      end
 
-  it "should be able to find right projects using :all, :first and :last" do
-    Unfuddler::Project.find(:all).should be_an_instance_of Array
-    Unfuddler::Project.find(:all).first.should be_an_instance_of Unfuddler::Project
+      it "should return an Array with Unfuddler::Project objects" do
+        Unfuddler::Project.find("testproject").first.should be_an_instance_of Unfuddler::Project
+      end
 
-    Unfuddler::Project.find(:first).should be_an_instance_of Unfuddler::Project
-    Unfuddler::Project.find(:first).should eql(Unfuddler::Project.find(:all).first)
+      it "should return Unfuddler::Project objects where the string passed corresponds to the name" do
+        Unfuddler::Project.find("testproject").first.short_name.should eql("testproject")
+      end
+    end
 
-    Unfuddler::Project.find(:first)
+    describe "given a hash argument" do
+      it "should return an Array" do
+        Unfuddler::Project.find(:id => 2).should be_an_instance_of Array
+      end
 
-    Unfuddler::Project.find(:last).should be_an_instance_of Unfuddler::Project
-    Unfuddler::Project.find(:last).should eql(Unfuddler::Project.find(:all).first)
-  end
+      it "should return an Array with Unfuddler::Project objects" do
+        Unfuddler::Project.find(:id => 2).first.should be_an_instance_of Unfuddler::Project
+      end
 
-  it "should find a project by id when passing a fixnum" do
-    Unfuddler::Project.find(2).should be_an_instance_of Array
-    Unfuddler::Project.find(2).first.should be_an_instance_of Unfuddler::Project
+      it "should return Unfuddler::Project objects where the object should correspond to the attributes specified in the query"
+        Unfuddler::Project.find(:id => 2).first.id.should eql? 2
+      end
+    end
+
+    # will awesomeify later!
+
+    it "should be able to find right projects using :all, :first and :last" do
+      Unfuddler::Project.find(:all).should be_an_instance_of Array
+      Unfuddler::Project.find(:all).first.should be_an_instance_of Unfuddler::Project
+
+      Unfuddler::Project.find(:first).should be_an_instance_of Unfuddler::Project
+      Unfuddler::Project.find(:first).should eql(Unfuddler::Project.find(:all).first)
+
+      Unfuddler::Project.find(:first)
+
+      Unfuddler::Project.find(:last).should be_an_instance_of Unfuddler::Project
+      Unfuddler::Project.find(:last).should eql(Unfuddler::Project.find(:all).first)
+    end
+
+    it "should find a project by id when passing a fixnum" do
+      Unfuddler::Project.find(2).should be_an_instance_of Array
+      Unfuddler::Project.find(2).first.should be_an_instance_of Unfuddler::Project
+    end
   end
 end
